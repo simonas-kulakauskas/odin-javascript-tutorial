eba3b6949a6daf874114f05fa2968871d6e26d03
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-17 03:57:13 +0100

    Changes made to index

---
+++ Variables and Operators/js/javascript.js
@@ -0,0 +1,49 @@
+/*
+console.log(23 + 30);
+console.log(10 + 20 + 30 + 40 + 50 + 60);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;*/
+
+
+// This portion is for Data Types & Conditionals
+
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const myName = "Simonas";
+const greeting = `Hello, ${myName}`;
+console.log(greeting);
+
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined);
+
+const greeting2 = "Hello";
+console.log(`${greeting2}, ${myName}`);
+
+const song = "'Me and Your Mama'";
+const score = 10;
+const highestScore = 10;
+const output = `I like the song ${song}, I gave it a score of ${(score / highestScore) * 100}%.`;
+console.log(output);
+
+const newLine = `One day you finally knew
+what you had to do, and began.`;
+console.log(newLine);
+
+const newLine2 = "One day you finally knew\nwhat you had to do, and began.";
+console.log(newLine2);
+
+const bigmouth = 'I\'ve got no right to take my place...';
+console.log(bigmouth);
+
+const name2 = "Front ";
+const number = 242;
+console.log(name2 + number);
